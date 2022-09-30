8f1ecbbf9541e838d43a7a0b3e4739e23d9005c7
Nikolay <nikolay.popurey@yandex.ru>
2022-09-30 12:06:19 +0300

    актуализация версии

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div>
+       <div>Кол-во лайков <strong>{{likes}}</strong></div>
+        <div>Кол-во дизлайков <strong>{{dislikes}}</strong></div>
+
+       <div>
+        <button type="submit" v-on:click="addLikes" >Лайк</button>
+        <button type="submit" @click = "addDislikes" >Дизлайк</button>
+       </div> 
+    </div>
 </template>
 
+<script>
+export default {
+    data(){
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLikes(){
+            this.likes += 1;
+        },
+        addDislikes(){
+            this.dislikes += 1;
+        }
+    },
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
